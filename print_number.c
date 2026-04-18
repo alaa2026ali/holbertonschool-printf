21190f6951c72547735ffbf80c58e2ac5315066b
alaa2026ali <ali_alaa18@icloud.com>
2026-04-18 05:03:07 +0300

    Fix negative number handling in print_number

--- print_number.c
+++ print_number.c
@@ -3,6 +3,7 @@
 /**
  * print_number - prints an integer
  * @n: number to print
+ *
  * Return: number of characters printed
  */
 int print_number(int n)
@@ -10,20 +11,22 @@ int print_number(int n)
 	int count = 0;
 	unsigned int num;
 
+	/* Handle negative numbers */
 	if (n < 0)
 	{
-		write(1, "-", 1);
-		count++;
-		num = -n;
+		count += write(1, "-", 1); /* print '-' */
+		num = (unsigned int)(-n);  /* convert to positive safely */
 	}
 	else
 	{
 		num = n;
 	}
 
+	/* Print digits recursively (left to right) */
 	if (num / 10)
 		count += print_number(num / 10);
 
+	/* Print last digit */
 	count += write(1, &"0123456789"[num % 10], 1);
 
 	return (count);
